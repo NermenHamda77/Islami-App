bcc0f345203ed196a48c094662391b77dc347d57
Nermen77 <nermeenzaitoon@gmail.com>
2025-07-12 15:50:39 +0300

    Complete Quran  tab.

--- lib/my_theme/my_theme_data.dart
+++ lib/my_theme/my_theme_data.dart
@@ -5,19 +5,18 @@ class MyTheme{
   static final ThemeData lightMode = ThemeData(
       primaryColor: AppColors.primaryLightColor,
     scaffoldBackgroundColor: Colors.transparent,
+
+    // ------------------------------------------------------------------------------
+
     appBarTheme: AppBarTheme(
       backgroundColor: Colors.transparent,
       elevation: 0,
       centerTitle: true,
 
     ),
-    textTheme: TextTheme(
-      bodyLarge: TextStyle(
-        fontSize: 30,
-        color: AppColors.blackColor,
-        fontWeight: FontWeight.bold
-      ),
-    ),
+
+    // ------------------------------------------------------------------------------
+
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       selectedIconTheme: IconThemeData(
         color: AppColors.blackColor,
@@ -25,6 +24,34 @@ class MyTheme{
       ),
       selectedItemColor: AppColors.blackColor
 
-    )
+    ),
+
+
+    // ------------------------------------------------------------------------------
+
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(
+          fontSize: 30,
+          color: AppColors.blackColor,
+          fontWeight: FontWeight.bold
+      ),
+
+      bodyMedium: TextStyle(
+          fontSize: 24,
+          color: AppColors.blackColor,
+          fontWeight: FontWeight.bold,
+      ),
+      bodySmall: TextStyle(
+          fontSize: 20,
+          color: AppColors.blackColor,
+          fontWeight: FontWeight.w700,
+
+      ),
+
+    ),
+
+    // ------------------------------------------------------------------------------
+
+
   );
 }
